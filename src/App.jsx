81f1b0a211c7d7b10291cc75602c729c6ce9e4a6
Abdul Wasi Bhat <bhatabdul75@gmail.com>
2025-03-22 20:05:31 +0530

    added Navbar & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <header>
-        <h1>Welcome to StudySync 📚</h1>
-        <p>Your all-in-one student productivity hub!</p>
-      </header>
+    <div>
+      <Navbar />
+      <div className="container">
+          <h1>Welcome to StudySync 📚</h1>
+          <p>Your all-in-one student productivity hub!</p>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>StudySync 📚</h1>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Tools</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
