ad96d08efb8a6e6c1f9e4831de80102b1855c1c6
Abdul Wasi Bhat <bhatabdul75@gmail.com>
2025-03-31 22:05:40 +0530

    improved attendance calculator

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AttendanceCalculator from "./components/AttendanceCalculator";
-import AttendanceRequirementCalculator from "./components/AttendanceRequirementCalculator";
 import Home from "./pages/Home";
 import Tools from "./pages/Tools";
 import About from "./pages/About";
@@ -22,7 +21,6 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="tools/attendance" element={<AttendanceCalculator />} />
-          <Route path="tools/attendance-requirement" element={<AttendanceRequirementCalculator />} />
         </Routes>
       </div>
       <Footer />
