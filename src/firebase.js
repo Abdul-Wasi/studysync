f0ab033cccf5d97f3c56fbeda8b02d70336f9570
Abdul Wasi <91587592+Abdul-Wasi@users.noreply.github.com>
2026-07-08 16:21:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL, // This points to Realtime Database
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL, // This points to Realtime Database.
 };
 
 const app = initializeApp(firebaseConfig);
